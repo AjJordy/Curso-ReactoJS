e3e03428b1423e1b71f7422a9dae27f0cd287016
AjJordy <jordyfaria0@gmail.com>
2024-01-16 14:30:24 -0300

    feat: StrictMode

--- github-explorer/src/index.tsx
+++ github-explorer/src/index.tsx
@@ -1,6 +1,10 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App'
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(<App />);
+const root = createRoot(document.getElementById('root')!);
+root.render(
+	<StrictMode>
+		<App />
+	</StrictMode>
+);
