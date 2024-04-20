11365f640f4706b1e7075ad0a6d6d78b56de6896
AjJordy <jordyfaria0@gmail.com>
2024-04-20 10:16:44 -0300

    feat: context api

--- dtmoney/src/components/Summary/index.tsx
+++ dtmoney/src/components/Summary/index.tsx
@@ -1,11 +1,23 @@
+import { useContext } from "react";
 import { Conteiner } from "./styles";
+import { TransactionsContext } from "../../TransactionsContext";
 import incomeImg from "../../assets/income.svg"
 import outcomeImg from "../../assets/outcome.svg"
 import totalImg from "../../assets/total.svg"
 
+
 export function Summary(){
+	const transactions = useContext(TransactionsContext);
+	
 	return (
 		<Conteiner>
+			{/* <TransactionsContext.Consumer>
+				{(data) => {
+					console.log('Hander props:', data)
+					return <></>
+				}}
+			</TransactionsContext.Consumer> */}
+
 			<div>
 				<header>
 					<p>Entradas</p>
